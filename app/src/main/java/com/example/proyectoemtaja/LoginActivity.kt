175c169bff53b668cd2347b21528221634aac664
matute <javier.matute.barragan@gmail.com>
2022-04-12 13:26:32 +0200

    Login 2.1

--- app/src/main/java/com/example/proyectoemtaja/LoginActivity.kt
+++ app/src/main/java/com/example/proyectoemtaja/LoginActivity.kt
@@ -1,14 +1,14 @@
 package com.example.proyectoemtaja
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.proyectoemtaja.databinding.ActivityLoginBinding
-import com.example.proyectoemtaja.databinding.ActivityMapsBinding
 import com.example.proyectoemtaja.service.APIService
+import com.example.proyectoemtaja.utilities.NullOnEmptyConverterFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,37 +19,40 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
 
-    private lateinit var etUser:EditText
-    private lateinit var etPassword:EditText
+
+    private lateinit var etEmail:EditText
+    private lateinit var etContrasenia:EditText
     private lateinit var textview:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        etPassword=binding.password
-        etUser=binding.user
+        etContrasenia=binding.etContrasenia
+        etEmail=binding.etEmail
         textview=binding.textView
+
+        binding.btnRegistrarse.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
 
         binding.btnLogin.setOnClickListener {
 
-            if(!etPassword.text.isBlank()&&!etUser.text.isBlank()){
+            if(!etContrasenia.text.isBlank()&&!etEmail.text.isBlank()){
                 CoroutineScope(Dispatchers.IO).launch {
                    var call= getRetrofit().create(APIService::class.java)
-                        .login(etUser.toString(), etPassword.toString())
+                        .login(etEmail.toString(), etContrasenia.toString())
 
                     runOnUiThread {
                         if (call.isSuccessful) {
-                            textview.text = call.body()
+                            textview.text = call.toString()
                         } else {
-                            Toast.makeText(
-                                this@LoginActivity,
-                                "fallo de conexion",
-                                Toast.LENGTH_LONG
-                            )
+                            Toast.makeText(this@LoginActivity, "fallo de conexion", Toast.LENGTH_LONG).show()
                         }
                     }
                 }
+            }else{
+                Toast.makeText(this, "Faltan campos por rellenar.", Toast.LENGTH_LONG).show()
             }
 
         }
@@ -58,7 +61,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder().baseUrl("http://192.168.1.34:8081/login/").addConverterFactory(
+        return Retrofit.Builder().baseUrl("http://192.168.1.34:8081").addConverterFactory(NullOnEmptyConverterFactory()).addConverterFactory(
             GsonConverterFactory.create()).build()
 
     }
